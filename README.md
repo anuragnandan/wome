3fe7419c0faad4aaa942a6f263d272af53e9c7ca
Anurag Raperthy <araperthy@advantone.com>
2018-04-02 16:17:41 -0400

    Making it work

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Recieve Home to Work and vice versa route notification with faster route every d
 ```
 composer install
 cp .env.example .env
+# Fill out proper values in .env
 php main.php
 ```
+
+### Requires
+- [Google Maps API](https://developers.google.com/maps/documentation/javascript/get-api-key)
+- [Push Me](https://pushme.jagcesar.se/)
--- main.php
+++ main.php
@@ -7,42 +7,49 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 $route = getRoute(getenv('HOUSE'), getenv('WORK'));
-echo $route;
+sendRoute($route);
 
-function getRoute($origin, $destination)
+function getResponse($type = 'GET', $url, $params = array())
 {
   $client = new Client();
   try{
-    $response = $client->request('GET', getenv('URL'), [
-      'query' => [
-        'origin' => $origin,
-        'destination' => $destination,
-        'key' => getenv('KEY')
-      ]
+    $response = $client->request($type, $url, [
+      'query' => $params
     ]);
 
-    $body = $response->getBody();
-    $body = json_decode($body);
-
-    if($body->status == 'OK')
-    {
-      $distance = $body->routes[0]->legs[0]->distance->text;
-      $duration = $body->routes[0]->legs[0]->duration->text;
-      $steps = $body->routes[0]->legs[0]->steps;
-
-      $wome = array();
-      foreach($steps as $turn => $step)
-      {
-        $wome[$turn] = $step->html_instructions;
-      }
-    }
-
-    return textRoute($distance, $duration, $wome);
+    return $response;
   }
   catch(Exception $e){
     echo $e->getMessage();
   }
+}
 
+function getRoute($origin, $destination)
+{
+   $params = [
+        'origin' => $origin,
+        'destination' => $destination,
+        'key' => getenv('KEY')
+      ];
+
+  $response = getResponse('GET', getenv('URL'), $params);
+  $body = $response->getBody();
+  $body = json_decode($body);
+
+  if($body->status == 'OK')
+  {
+    $distance = $body->routes[0]->legs[0]->distance->text;
+    $duration = $body->routes[0]->legs[0]->duration->text;
+    $steps = $body->routes[0]->legs[0]->steps;
+
+    $wome = array();
+    foreach($steps as $turn => $step)
+    {
+      $wome[$turn] = $step->html_instructions;
+    }
+  }
+
+  return textRoute($distance, $duration, $wome);
 }
 
 function textRoute($distance = 0, $duration = 0, $wome = array())
@@ -52,5 +59,14 @@ function textRoute($distance = 0, $duration = 0, $wome = array())
     $text_route .= ($turn + 1).". ".strip_tags($step).".\n";
 
   return $text_route;
+}
+
+function sendRoute($route)
+{
+  $params = [
+    'title' => $route,
+    'identifier' => getenv('NOTIFY_KEY')
+  ];
+  $response = getResponse('GET', getenv('NOTIFY_URL'), $params);
 }
 ?>
