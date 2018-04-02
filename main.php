fae57faad5efd888550b7e63b53c62f2da75e9f8
Anurag Raperthy <araperthy@advantone.com>
2018-04-02 14:48:17 -0400

    Making it work

--- main.php
+++ main.php
@@ -6,7 +6,8 @@ use GuzzleHttp\Client;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-getRoute(getenv('HOUSE'), getenv('WORK'));
+$route = getRoute(getenv('HOUSE'), getenv('WORK'));
+echo $route;
 
 function getRoute($origin, $destination)
 {
@@ -20,10 +21,36 @@ function getRoute($origin, $destination)
       ]
     ]);
 
+    $body = $response->getBody();
+    $body = json_decode($body);
+
+    if($body->status == 'OK')
+    {
+      $distance = $body->routes[0]->legs[0]->distance->text;
+      $duration = $body->routes[0]->legs[0]->duration->text;
+      $steps = $body->routes[0]->legs[0]->steps;
+
+      $wome = array();
+      foreach($steps as $turn => $step)
+      {
+        $wome[$turn] = $step->html_instructions;
+      }
+    }
+
+    return textRoute($distance, $duration, $wome);
   }
   catch(Exception $e){
     echo $e->getMessage();
   }
 
+}
+
+function textRoute($distance = 0, $duration = 0, $wome = array())
+{
+  $text_route = "Distance: $distance and will take $duration.\n";
+  foreach($wome as $turn => $step)
+    $text_route .= ($turn + 1).". ".strip_tags($step).".\n";
+
+  return $text_route;
 }
 ?>
